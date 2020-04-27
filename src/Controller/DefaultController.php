0c4d655b8e7a5424de12ec556ca57b2f90abdcc1
yura.pidkova <yurapidkova@gmail.com>
2020-04-27 20:16:03 +0300

    redis options

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,15 +8,27 @@ class DefaultController
 {
     public function call()
     {
-       	$ch = curl_init();
 
-		curl_setopt($ch, CURLOPT_URL,"http://auth_nginx/test");
+//        $a = new \Redis();
+//        $a->connect('redis');
+//        $b = $a->ping('asd');
+//        echo $b;
+//        die();
+
+        $ch = curl_init();
+
+        $postParams = ['name' => "test", "password" => '888', 'network' => '1', 'skin' => '2'];
+
+		curl_setopt($ch, CURLOPT_URL,"http://auth_nginx/auth");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $postParams);
 
-		$server_output = curl_exec($ch);
-		$info = curl_getinfo($ch);
-		curl_close ($ch);
-		return new Response(
+        $server_output = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        curl_close ($ch);
+//      dd($server_output);
+        return new Response(
             $server_output
         );
     }
